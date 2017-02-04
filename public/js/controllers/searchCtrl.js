7b82eb19a06f71809697a29597eb2c27d73f4fe1
윤송이 <yunsong-i@yunsong-iui-MacBook-Pro.local>
2017-02-04 21:21:24 +0900

    front end0

--- public/js/controllers/searchCtrl.js
+++ public/js/controllers/searchCtrl.js
@@ -2,6 +2,9 @@ angular.module('searchCtrl', [])
 	.controller('searchController', function($scope, searchService) {
 
 		$scope.keyword = '';
+		$scope.search = function(nickName){
+			searchService.search(nickName);
+		};
 
 
 	});
--- public/js/services/searchService.js
+++ public/js/services/searchService.js
@@ -1,5 +1,18 @@
-angular.module('Service', []).factory('searchService', ['$http', function($http) {
+angular.module('searchService', [])
+	.factory('nickName', function($http) {
+		let search = function(nickName){
+			return $http({
+				method : 'GET',
+				url : '/search',
+				params : { keyword :nickName }
+			})
+			.then(function(res){
+				return res.data;
+			});
+		}
 
+		return {
+			search : search
+		};	
 	
-
-}]);
+	});
